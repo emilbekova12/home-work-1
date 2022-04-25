38f2e937b973884bf52b936e1684610f3f4df6d2
emilbekova12 <104209064+emilbekova12@users.noreply.github.com>
2022-04-25 14:08:48 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  String address = 'suumbaeva st.,Bishkek';
+  print(address);
+  int age = 20;
+  double weight = 63.2;
+  String name = 'Daniyar';
+  print(age, weight, name);
+  int a = 20;
+  String b = '241292';
+  a == int.parse(a.toString()
+  print(a);
+}
